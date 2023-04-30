c5524e39d8ae2ef1d83070935f3598f60bf4ea43
shiwasu-tech <njayamena@gmail.com>
2023-04-30 15:57:39 +0900

    add: functions fix: functions

--- M5key/M5key.ino
+++ M5key/M5key.ino
@@ -37,7 +37,7 @@ int BTN1 = 26;
 int BTN2 = 25;
 
 int mode = 1;
-int keynum = 0;
+int keynum = 1;
 
 int keytemp = 0;
 
@@ -71,29 +71,17 @@ void KeySend(int mode,int com){
   }else if(com == 131){
     BleKeyboard.press(KEY_MEDIA_PLAY_PAUSE);
   }else if(com == 241){
-    BleKeyboard.press(KEY_RIGHT_ARROW);
-    BleKeyboard.press(KEY_RIGHT_ARROW);
-    BleKeyboard.press(KEY_RIGHT_ARROW);
-    BleKeyboard.press(KEY_RIGHT_ARROW);
-    BleKeyboard.press(KEY_RIGHT_ARROW);
+    BleKeyboard.press(KEY_DOWN_ARROW);
+    delay(900);
   }else if(com == 242){
-    BleKeyboard.press(KEY_LEFT_ARROW);
-    BleKeyboard.press(KEY_LEFT_ARROW);
-    BleKeyboard.press(KEY_LEFT_ARROW);
-    BleKeyboard.press(KEY_LEFT_ARROW);
-    BleKeyboard.press(KEY_LEFT_ARROW);
+    BleKeyboard.press(KEY_UP_ARROW);
+    delay(900);
   }else if(com == 243){
-    BleKeyboard.press(KEY_DOWN_ARROW);
-    BleKeyboard.press(KEY_DOWN_ARROW);
-    BleKeyboard.press(KEY_DOWN_ARROW);
-    BleKeyboard.press(KEY_DOWN_ARROW);
-    BleKeyboard.press(KEY_DOWN_ARROW);
+    BleKeyboard.press(KEY_LEFT_ARROW);
+    delay(900);
   }else if(com == 244){
-    BleKeyboard.press(KEY_DOWN_ARROW);
-    BleKeyboard.press(KEY_DOWN_ARROW);
-    BleKeyboard.press(KEY_DOWN_ARROW);
-    BleKeyboard.press(KEY_DOWN_ARROW);
-    BleKeyboard.press(KEY_DOWN_ARROW);
+    BleKeyboard.press(KEY_RIGHT_ARROW);
+    delay(900);
   }else if(com == 251){
     BleKeyboard.press(KEY_LEFT_ALT);
     BleKeyboard.press(KEY_RIGHT_ARROW);
@@ -105,46 +93,84 @@ void KeySend(int mode,int com){
   }else if(com == 254){
     //BleKeyboard.press(KEY_)
   }else if(com == 231){
-    BleKeyboard.press(KEY_F5);
+    BleKeyboard.write(KEY_F5);
   }else if(com == 341){
-    BleKeyboard.press(KEY_MEDIA_NEXT_TRACK);
+    BleKeyboard.write(KEY_MEDIA_NEXT_TRACK);
   }else if(com == 342){
-    BleKeyboard.press(KEY_MEDIA_PREVIOUS_TRACK);
+    BleKeyboard.write(KEY_MEDIA_PREVIOUS_TRACK);
   }else if(com == 343){
-    BleKeyboard.press(KEY_ESC);
-    BleKeyboard.press(KEY_ESC);
-    BleKeyboard.release(KEY_ESC);
-    BleKeyboard.print("f");
-  }else if(com == 344){
     BleKeyboard.press(KEY_LEFT_SHIFT);
     BleKeyboard.print("f");
     BleKeyboard.release(KEY_LEFT_SHIFT);
+    BleKeyboard.press(KEY_ESC);
+    BleKeyboard.release(KEY_ESC);
+  }else if(com == 344){
+    BleKeyboard.print("f");
     BleKeyboard.press(KEY_F11);
   }else if(com == 351){
-    //BleKeyboard.press(KEY_);
+    BleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
   }else if(com == 352){
-    //BleKeyboard.press(KEY_);
+    BleKeyboard.write(KEY_MEDIA_VOLUME_UP);
   }else if(com == 353){
-    BleKeyboard.press(KEY_MEDIA_VOLUME_DOWN);
+    //BleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
   }else if(com == 354){
-    BleKeyboard.press(KEY_MEDIA_VOLUME_UP);
+    //BleKeyboard.write(KEY_MEDIA_VOLUME_UP);
   }else if(com == 331){
-    BleKeyboard.press(KEY_MEDIA_PLAY_PAUSE);
-  }
+    BleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
+  }else if(com == 441){
+    BleKeyboard.write(KEY_RIGHT_ARROW);
+  }else if(com == 442){
+    BleKeyboard.write(KEY_LEFT_ARROW);
+  }else if(com == 443){
+    BleKeyboard.write(KEY_DOWN_ARROW);
+  }else if(com == 444){
+    BleKeyboard.write(KEY_UP_ARROW);
+  }else if(com == 451){
+    BleKeyboard.press(KEY_LEFT_ALT);
+    BleKeyboard.press(KEY_RIGHT_ARROW);
+  }else if(com == 452){
+    BleKeyboard.press(KEY_LEFT_ALT);
+    BleKeyboard.press(KEY_LEFT_ARROW);
+  }else if(com == 453){
+    //BleKeyboard.write(KEY_);
+  }else if(com == 454){
+    //BleKeyboard.write(KEY_);
+  }else if(com == 401){
+    BleKeyboard.write(KEY_MEDIA_PLAY_PAUSE);
+  }else if(com == 402){
+    BleKeyboard.print("f");
+  }else if(com == 403){
+    BleKeyboard.write(KEY_RETURN);
+  }else if(com == 404){
+    BleKeyboard.write(KEY_F5);
+  }else if(com == 405){
+    BleKeyboard.press(KEY_LEFT_ALT);
+    BleKeyboard.press(KEY_TAB);
+  }else if(com == 406){
+    //BleKeyboard.write(KEY_);
+  }else if(com == 407){
+    //BleKeyboard.write(KEY_);
+  }else if(com == 408){
+    //BleKeyboard.write(KEY_);
+  }else if(com == 409){
+    //BleKeyboard.write(KEY_);
+  }else if(com == 410){
+    //BleKeyboard.write(KEY_);
+  }           
 
 
   delay(100);
   BleKeyboard.releaseAll();
-  delay(100);
+  delay(300);
 }
 
 void UIinit(int mode,int key){
 
   //mode UI arrow init
-  M5.Lcd.fillTriangle(36,25,36,33,43,29,DARKGREY);
-  M5.Lcd.fillTriangle(36,40,36,48,43,44,DARKGREY);
-  M5.Lcd.fillTriangle(36,55,36,63,43,59,DARKGREY);
+  M5.Lcd.fillTriangle(36,30,36,38,43,34,DARKGREY);
+  M5.Lcd.fillTriangle(36,50,36,58,43,54,DARKGREY);
   M5.Lcd.fillTriangle(36,70,36,78,43,74,DARKGREY);
+  M5.Lcd.fillTriangle(36,90,36,98,43,94,DARKGREY);
 
   ////keyUI init
 
@@ -203,15 +229,15 @@ void UIinit(int mode,int key){
   M5.Display.print("Alt + Tab");
 
   if(mode == 1){
-    M5.Lcd.fillTriangle(36,25,36,33,43,29,RED);
+    M5.Lcd.fillTriangle(36,30,36,38,43,34,RED);
     //arrow
-    M5.Lcd.fillTriangle(145,48,155,43,155,53,RED);
-    M5.Lcd.fillTriangle(175,43,175,53,185,48,RED);
+    M5.Lcd.fillTriangle(145,48,155,43,155,53,BLUE);
+    M5.Lcd.fillTriangle(175,43,175,53,185,48,BLUE);
     //play/pause
-    M5.Lcd.fillTriangle(200,33,200,37,204,35,RED);
-    M5.Lcd.drawLine(207,32,205,38,RED);
-    M5.Lcd.fillRect(209,34,2,4,RED);
-    M5.Lcd.fillRect(213,34,2,4,RED);
+    M5.Lcd.fillTriangle(200,33,200,37,204,35,GREEN);
+    M5.Lcd.drawLine(207,32,205,38,GREEN);
+    M5.Lcd.fillRect(209,34,2,4,GREEN);
+    M5.Lcd.fillRect(213,34,2,4,GREEN);
     //alt + tab
     M5.Lcd.setTextColor(RED,BLACK);
     M5.Lcd.setCursor(170,92);
@@ -219,19 +245,19 @@ void UIinit(int mode,int key){
   }
 
   if(mode == 2){
-    M5.Lcd.fillTriangle(36,40,36,48,43,44,RED);
+    M5.Lcd.fillTriangle(36,50,36,58,43,54,RED);
     //arrow
-    M5.Lcd.fillTriangle(165,30,160,40,170,40,RED);
-    M5.Lcd.fillTriangle(160,43,170,43,165,53,RED);
+    M5.Lcd.fillTriangle(165,30,160,40,170,40,BLUE);
+    M5.Lcd.fillTriangle(160,43,170,43,165,53,BLUE);
     //go / back : browse-mode
-    M5.Lcd.drawCircle(156,65,7,RED);
-    M5.Lcd.drawLine(152,65,160,65,RED);
-    M5.Lcd.drawLine(156,69,152,65,RED);
-    M5.Lcd.drawLine(156,61,152,65,RED);
-    M5.Lcd.drawCircle(174,65,7,RED);
-    M5.Lcd.drawLine(170,65,178,65,RED);
-    M5.Lcd.drawLine(174,69,178,65,RED);
-    M5.Lcd.drawLine(174,61,178,65,RED);
+    M5.Lcd.drawCircle(156,65,7,GREEN);
+    M5.Lcd.drawLine(152,65,160,65,GREEN);
+    M5.Lcd.drawLine(156,69,152,65,GREEN);
+    M5.Lcd.drawLine(156,61,152,65,GREEN);
+    M5.Lcd.drawCircle(174,65,7,GREEN);
+    M5.Lcd.drawLine(170,65,178,65,GREEN);
+    M5.Lcd.drawLine(174,69,178,65,GREEN);
+    M5.Lcd.drawLine(174,61,178,65,GREEN);
     //F5
     M5.Lcd.setTextColor(RED,BLACK);
     M5.Lcd.setCursor(145,92);
@@ -239,39 +265,77 @@ void UIinit(int mode,int key){
   }
 
   if(mode == 3){
-    M5.Lcd.fillTriangle(36,55,36,63,43,59,RED);
+    M5.Lcd.fillTriangle(36,70,36,78,43,74,RED);
     //play/pause
     M5.Lcd.fillTriangle(200,33,200,37,204,35,RED);
     M5.Lcd.drawLine(207,32,205,38,RED);
     M5.Lcd.fillRect(209,34,2,4,RED);
     M5.Lcd.fillRect(213,34,2,4,RED);
     //next
-    M5.Lcd.drawLine(193,45,193,51,RED);
-    M5.Lcd.fillTriangle(199,45,199,51,193,48,RED);
-    M5.Lcd.fillTriangle(205,45,205,51,199,48,RED);
+    M5.Lcd.drawLine(193,45,193,51,BLUE);
+    M5.Lcd.fillTriangle(199,45,199,51,193,48,BLUE);
+    M5.Lcd.fillTriangle(205,45,205,51,199,48,BLUE);
     //previous
-    M5.Lcd.fillTriangle(209,45,209,51,215,48,RED);
-    M5.Lcd.fillTriangle(215,45,215,51,221,48,RED);
-    M5.Lcd.drawLine(221,45,221,51,RED);
+    M5.Lcd.fillTriangle(209,45,209,51,215,48,BLUE);
+    M5.Lcd.fillTriangle(215,45,215,51,221,48,BLUE);
+    M5.Lcd.drawLine(221,45,221,51,BLUE);
     //volume up / down
-    M5.Lcd.fillRect(194,63,4,4,RED);
-    M5.Lcd.fillTriangle(194,65,200,61,200,68,RED);
-    M5.Lcd.drawLine(202,64,202,66,RED);
-    M5.Lcd.fillRect(209,63,4,4,RED);
-    M5.Lcd.fillTriangle(209,65,213,61,213,68,RED);
-    M5.Lcd.drawLine(215,64,215,66,RED); 
-    M5.Lcd.drawLine(217,63,217,67,RED);
-    M5.Lcd.drawLine(219,62,219,68,RED);
+    M5.Lcd.fillRect(194,63,4,4,GREEN);
+    M5.Lcd.fillTriangle(194,65,200,61,200,68,GREEN);
+    M5.Lcd.drawLine(202,64,202,66,GREEN);
+    M5.Lcd.fillRect(209,63,4,4,GREEN);
+    M5.Lcd.fillTriangle(209,65,213,61,213,68,GREEN);
+    M5.Lcd.drawLine(215,64,215,66,GREEN); 
+    M5.Lcd.drawLine(217,63,217,67,GREEN);
+    M5.Lcd.drawLine(219,62,219,68,GREEN);
   }
 
   if(mode == 4){
-    M5.Lcd.fillTriangle(36,70,36,78,43,74,RED);
+    M5.Lcd.fillTriangle(36,90,36,98,43,94,RED);
+
+    //arrow
+    M5.Lcd.fillTriangle(145,48,155,43,155,53,BLUE);
+    M5.Lcd.fillTriangle(165,30,160,40,170,40,BLUE);
+    M5.Lcd.fillTriangle(160,43,170,43,165,53,BLUE);
+    M5.Lcd.fillTriangle(175,43,175,53,185,48,BLUE);
+
+    //go / back : browse-mode
+    M5.Lcd.drawCircle(156,65,7,GREEN);
+    M5.Lcd.drawLine(152,65,160,65,GREEN);
+    M5.Lcd.drawLine(156,69,152,65,GREEN);
+    M5.Lcd.drawLine(156,61,152,65,GREEN);
+    M5.Lcd.drawCircle(174,65,7,GREEN);
+    M5.Lcd.drawLine(170,65,178,65,GREEN);
+    M5.Lcd.drawLine(174,69,178,65,GREEN);
+    M5.Lcd.drawLine(174,61,178,65,GREEN);
+
     if(key == 1){
       //play/pause
       M5.Lcd.fillTriangle(200,33,200,37,204,35,RED);
       M5.Lcd.drawLine(207,32,205,38,RED);
       M5.Lcd.fillRect(209,34,2,4,RED);
       M5.Lcd.fillRect(213,34,2,4,RED);
+    }else if(key == 2){
+      //fullscreen
+      M5.Lcd.fillRect(156,76,15,10,RED);
+      M5.Lcd.fillRect(159,78,9,6,BLACK);
+      M5.Lcd.fillRect(156,79,15,4,BLACK);
+      M5.Lcd.fillRect(161,76,5,10,BLACK);
+    }else if(key == 3){
+      //Enter
+      M5.Lcd.setTextColor(RED,BLACK);
+      M5.Lcd.setCursor(188,77);
+      M5.Display.print("Enter");
+    }else if(key == 4){
+      //F5
+      M5.Lcd.setTextColor(RED,BLACK);
+      M5.Lcd.setCursor(145,92);
+      M5.Display.print("F5");
+    }else if(key == 5){
+      //alt + tab
+      M5.Lcd.setTextColor(RED,BLACK);
+      M5.Lcd.setCursor(170,92);
+      M5.Display.print("Alt + Tab");
     }
   }
 
@@ -381,24 +445,37 @@ void setup() {
   M5.Lcd.setTextColor(WHITE,BLACK);
 
   //mode UI draw
-  M5.Lcd.setCursor(10, 25);
+  M5.Lcd.setCursor(10, 30);
   M5.Display.print("Mode");
-  M5.Lcd.setCursor(50, 25);
+  M5.Lcd.setCursor(50, 30);
   M5.Display.print("Presentation");
-  M5.Lcd.setCursor(50, 40);
+  M5.Lcd.setCursor(50, 50);
   M5.Display.print("Browsing");
-  M5.Lcd.setCursor(50, 55);
-  M5.Display.print("Media");
   M5.Lcd.setCursor(50, 70);
+  M5.Display.print("Media");
+  M5.Lcd.setCursor(50, 90);
   M5.Display.print("Selectable");
   //M5.Lcd.setCursor(50, 85);
   //M5.Display.print("Phone");
   //M5.Lcd.setCursor(50, 100);
   //M5.Display.print("Mouse");
 
+  //Manual
+  M5.Lcd.fillRect(32,106,8,8,BLUE);
+  M5.Lcd.setCursor(41,106);
+  M5.Display.print("...Blue -Button + Motion.");
+  M5.Lcd.fillRect(32,116,8,8,GREEN);
+  M5.Lcd.setCursor(41,116);
+  M5.Display.print("...Green-Button + Motion.");
+  M5.Lcd.fillRect(32,126,8,8,RED);
+  M5.Lcd.setCursor(41,126);
+  M5.Display.print("...Blue -Button + Green-Button.");
+  
+
   //pertition draw
   M5.Lcd.drawLine(0,21,240,21,WHITE);
-  M5.Lcd.drawLine(135,22,135,135,DARKGREY);
+  M5.Lcd.drawLine(135,22,135,104,DARKGREY);
+  M5.Lcd.drawLine(0,103,240,103,DARKGREY);
 
   //UI initialize
   UIinit(1,0);
@@ -419,7 +496,7 @@ void loop() {
     }
 
     keynum = 0;
-    UIinit(mode,0);
+    UIinit(mode,1);
     delay(100);
 
   }
@@ -427,10 +504,11 @@ void loop() {
   if(mode == 4){
     if(M5.BtnA.isPressed()){
       keynum += 1;
-      if(keynum > 10){
-        keynum = 0;
+      if(keynum > 5){
+        keynum = 1;
       }
       UIinit(mode,keynum);
+      delay(100);
     }
   }
 
