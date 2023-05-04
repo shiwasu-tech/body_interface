ef58b6bb75eecd32653c0413651c809f17e7b949
shiwasu-tech <njayamena@gmail.com>
2023-05-04 09:30:27 +0900

    add: edited .gitignore add: TEST/ add: M5mouse_joy

--- M5mouse_joy/M5mouse_joy.ino
+++ M5mouse_joy/M5mouse_joy.ino
@@ -1,6 +1,9 @@
-
 #include <M5Unified.h>
 #include <BleMouse.h>
+#include <Wire.h>
+#define JOY_ADDR 0x52
+
+static int x,y,btn;
 
 const int BTN1 = 26;
 const int BTN2 = 25;
@@ -19,11 +22,7 @@ BleMouse BleMouse("M5StickCPlus-mouse");
 
 void MsSend(int mode,int com){
   com = mode * 100 + com;
-  if(com == 141){
-    BleKeyboard.press(KEY_RIGHT_ARROW);
-  }else if(com == 142){
-    BleKeyboard.press(KEY_LEFT_ARROW);
-  }
+  
 }
 
 void UIinit(int mode,int key){
@@ -136,31 +135,20 @@ void setup() {
   M5.Lcd.setTextColor(WHITE,BLACK);
 
   //mode UI draw
-  M5.Lcd.setCursor(10, 30);
-  M5.Display.print("Mode");
-  M5.Lcd.setCursor(50, 30);
-  M5.Display.print("Presentation");
-  M5.Lcd.setCursor(50, 50);
-  M5.Display.print("Browsing");
-  M5.Lcd.setCursor(50, 70);
-  M5.Display.print("Media");
-  M5.Lcd.setCursor(50, 90);
-  M5.Display.print("Selectable");
-  //M5.Lcd.setCursor(50, 85);
-  //M5.Display.print("Phone");
-  //M5.Lcd.setCursor(50, 100);
-  //M5.Display.print("Mouse");
+  /*
+    M5.Lcd.setCursor(10, 30);
+    M5.Display.print("Mode");
+    M5.Lcd.setCursor(50, 30);
+    M5.Display.print("Presentation");
+    M5.Lcd.setCursor(50, 50);
+    M5.Display.print("Browsing");
+    M5.Lcd.setCursor(50, 70);
+    M5.Display.print("Media");
+    M5.Lcd.setCursor(50, 90);
+    M5.Display.print("Selectable");
+    */
 
-  //Manual
-  M5.Lcd.fillRect(32,106,8,8,BLUE);
-  M5.Lcd.setCursor(41,106);
-  M5.Display.print("...Blue -Button + Motion.");
-  M5.Lcd.fillRect(32,116,8,8,GREEN);
-  M5.Lcd.setCursor(41,116);
-  M5.Display.print("...Green-Button + Motion.");
-  M5.Lcd.fillRect(32,126,8,8,RED);
-  M5.Lcd.setCursor(41,126);
-  M5.Display.print("...Blue -Button + Green-Button.");
+  //manual
   
 
   //pertition draw
@@ -171,12 +159,38 @@ void setup() {
   //UI initialize
   UIinit(1,0);
 
-  BleKeyboard.begin();
-
+  BleMouse.begin();
+  Wire.begin(32, 33);  
   M5.Imu.begin();
                                
 }
 
 void loop() {
+  Wire.requestFrom(JOY_ADDR, 3);
+  if(Wire.available()) { //If data is received.
+    
+    x = Wire.read();
+    y = Wire.read();
+    btn = Wire.read();
+
+    x -= 130;
+    y -= 130;
+
+  }
+
   
+  if(btn == 0){
+    BleMouse.release(MOUSE_MIDDLE);
+    if(x>10||x<-10||y>10||y<-10){
+      BleMouse.move(-x/10,y/10);
+
+    }
+  }else{
+    if(x>10||x<-10||y>10||y<-10){
+      BleMouse.press(MOUSE_MIDDLE);
+      BleMouse.move(-x/10,y/10);
+
+    }
+  }
+  delay(10);
 }
---
+++ memo.md
@@ -0,0 +1,57 @@
+腕ふりとデータ
+
+手のひら水平
+pitch roll yaw == ??
+
+手のひらを水平 && 上下に振る
+
+gyroX -200 ~ 200
+gyroY -100 ~ 100
+gyroZ ??
+
+手のひら水平 && 左右に振る
+
+gyroX -100 ~ 100
+gyroY ??
+gyroZ -200 ~ 200
+
+手首ねじる
+
+pitch == ??
+roll yaw -80 ~ 80
+
+gyroX ??
+gyroY -400 ~ 400
+gyroZ ??
+
+手のひら垂直(右手)
+pitch ??
+roll -80~-60
+yaw -80, 80
+
+rollが常に負?
+yawが常に極値?
+
+
+手のひら垂直 && 上下に振る
+
+gyroX ??
+gyroY ??
+gyroZ -200 ~ 200
+
+手のひら垂直 && 左右に振る
+
+gyroX -200 ~ 200
+gyroY ??
+gyroZ ??
+
+
+
+
+
+joystick
+押下の閾値
+
+x ~100 / 160~ 
+y ~100 / 160~
+b 1/0
